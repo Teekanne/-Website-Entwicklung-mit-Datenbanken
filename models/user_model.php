e5e9d6a5099b4add3eb7cf42b70c2eb8a0ca1a4e
Ben <Ben@193.175.177.25>
2016-06-17 16:01:52 +0200

    fixxed some bugs

--- models/user_model.php
+++ models/user_model.php
@@ -10,14 +10,14 @@ class User_Model extends Model
 
 	public function userList()
 	{
-		$sth = $this->db->prepare('Select ID, TITLE, FIRSTNAME, LASTNAME, EMAIL, ROLE1 from T_TUTOR');
+		$sth = $this->db->prepare('Select ID, TITLE, FIRSTNAME, LASTNAME, EMAIL, ROLE from T_TUTOR');
                 $sth->execute();
                 return $sth->fetchAll();
 		
 	}
 	
         public function create($data){
-                $sth = $this->db->prepare('INSERT INTO T_TUTOR(`TITLE`, `FIRSTNAME`, `LASTNAME`, `EMAIL`, `PASSWORD`, `ROLE`, `FK_STATE`, `FK_DEPARTMENT`, `FK_ROLE`)VALUES (:TITLE, :FIRSTNAME, :LASTNAME, :EMAIL, :PASSWORD, :ROLE1, :FK_STATE, :FK_DEPARTMENT, :FK_ROLE)');
+                $sth = $this->db->prepare('INSERT INTO T_TUTOR(`TITLE`, `FIRSTNAME`, `LASTNAME`, `EMAIL`, `PASSWORD`, `ROLE`, `FK_STATE`, `FK_DEPARTMENT`, `FK_ROLE`)VALUES (:TITLE, :FIRSTNAME, :LASTNAME, :EMAIL, :PASSWORD, :ROLE, :FK_STATE, :FK_DEPARTMENT, :FK_ROLE)');
                 $sth->execute(array(
         ':TITLE' => $data['TITLE'],
         ':FIRSTNAME' =>  $data['FIRSTNAME'],
