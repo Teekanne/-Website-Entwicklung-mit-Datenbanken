4e325d9cd0152bae66de0382a528dc19e6ee26b3
chpo7234 <pohl1991@yahoo.de>
2016-06-23 15:06:47 +0200

    Update index.php

--- views/newquestion/index.php
+++ views/newquestion/index.php
@@ -1,4 +1,65 @@
 <?php
+
+    class Category {
+        /* ID CATNAME LEVEL FK_TUTOR FK_PARENT_ID*/
+        private $id;
+        private $catname;
+        private $level;
+        private $fkTutor;
+        private $fkParentId;
+        private $dataBase;
+        private $pdo;
+        
+        public function __get($key){
+            if(property_exists($this, $key)){
+                return $this->$key;                
+            }
+        }
+        
+        public function __set($key, $value){
+            if(property_exists($this, $key)){
+                $this->$key = $value;
+            }
+        }
+
+        public function __construct($id, $catname, $level, $fkTutor, $fkParentId){
+            $this->id = $id;
+            $this->catname = $catname;
+            $this->level = $level;
+            $this->fkTutor = $fkTutor;
+            $this->fkParentId = $fkParentId;
+            $this->pdo = new PDO('mysql:host=projekt.wi.fh-flensburg.de;dbname=projekt2015a', 'projekt2015a', 'P2016s7');
+        }
+        
+        public static function Load($name) {
+            $db = new DataBase();
+            $column = $db->SelectRow("SELECT * FROM T_CATEGORY WHERE CATNAME='" . $name . "'");
+            return new Category($column["ID"], $column["CATNAME"], $column["LEVEL"], $column["FK_TUTOR"], $column["FK_PARENT_ID"]);
+        }
+        
+        /* Gibt eine Liste des generischen Typs "Category" zurück */
+        public static function GetFirstLevelCategories(){
+            $pdo = new PDO('mysql:host=projekt.wi.fh-flensburg.de;dbname=projekt2015a', 'projekt2015a', 'P2016s7');
+            $result = $pdo->query("SELECT * FROM T_CATEGORY WHERE LEVEL=1 ORDER BY ID");
+            $categories = array();
+            foreach($result as $r => $category){
+                array_push($categories, new Category($category["ID"], $category["CATNAME"], $category["LEVEL"], $category["FK_TUTOR"], $category["FK_PARENT_ID"]));
+            }
+            return $categories;
+        }
+        
+        public function GetSecondLevelCategories(){
+            $pdo = new PDO('mysql:host=projekt.wi.fh-flensburg.de;dbname=projekt2015a', 'projekt2015a', 'P2016s7');
+            $result = $pdo->query("SELECT * FROM T_CATEGORY WHERE FK_PARENT_ID=" . $this->id);
+            $categories = array();
+            foreach($result as $r => $category){
+                array_push($categories, new Category($category["ID"], $category["CATNAME"], $category["LEVEL"], $category["FK_TUTOR"], $category["FK_PARENT_ID"]));
+            }
+            return $categories;            
+        }
+    }
+    
+
     function ShowSelectBoxWithCategories(){
         echo "<select name='category'>";
         foreach(Category::GetFirstLevelCategories() as $c => $category){
