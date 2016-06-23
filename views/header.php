5f3c9cee17f4a6d177600f49788b8633d162c427
chpo7234 <pohl1991@yahoo.de>
2016-06-23 15:29:22 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -6,6 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 		<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
 		<script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
+		<script type="text/javascript" src="newquestion/js/javascript.js"></script>
+		
 		<?php
 			if (isset($this->js)) 
 			{
