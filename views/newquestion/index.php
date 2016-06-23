700f2dc04f449a158ef07738e53a7955ac0ec533
chpo7234 <pohl1991@yahoo.de>
2016-06-23 14:59:30 +0200

    Update index.php

--- views/newquestion/index.php
+++ views/newquestion/index.php
@@ -1,9 +1,56 @@
-<h1>Fragen erstellen Platzhalter</h1>
+<?php include("../header.php"); ?>
+
 <?php
+    function ShowSelectBoxWithCategories(){
+        echo "<select name='category'>";
+        foreach(Category::GetFirstLevelCategories() as $c => $category){
+            echo "<option>" . $category->__get("catname") . "</option>";
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+            foreach($category->GetSecondLevelCategories() as $s => $secondLevelCategory){
+                echo "<option>- " . $secondLevelCategory->__get("catname") . "</option>";
+            }
+        }        
+        
+        echo "</select>";
+    }
+?>
 
+
+<h2>Neue Umfrage erstellen</h2>
+<form action="frmSaveQuestion.php" method="post">
+    <table id="questionTable">
+        <tr>
+            <th>Kategorie</th>
+            <th><?php ShowSelectBoxWithCategories(); ?></th>
+        </tr>
+        <?php for($i=0;$i<20;$i++){ echo "<tr></tr>"; } ?>
+        <tr>
+            <th>Frage</th>
+            <th><input type="text" name="question1"/></th>
+        </tr>
+        <tr>
+            <th>Beschreibung</th>
+            <th><textarea name="description" maxlength="1000"></textarea></th>
+        </tr>
+        <tr>
+            <th>Antworten</th>
+            <th> 
+                <div id="divAnswers1">
+                    <input type="text" name="answer[]" id="answer1_1" placeholder="Antwortmöglichkeit 1" /><br />
+                    <input type="text" name="answer[]" id="answer1_2" placeholder="Antwortmöglichkeit 2" onkeydown="addTextbox('divAnswers1', this);" />
+                </div>
+            </th>
+        </tr>
+        <tr>
+            <th>Art</th>
+            <th>
+                <input type="radio" name="choice1" value="singlechoice" checked="checked"/><label>Single-Choice</label><br />
+                <input type="radio" name="choice1" value="multiplechoice"/><label>Multiple-Choice</label>
+            </th>                            
+        </tr>            <th>Beschreibung</th>
+    </table>
+    
+    <input type="button" value="Neue Frage einfügen" onclick="addNewQuestion('questionTable', 'question1');">  <input type="submit" value="Jetzt erstellen &raquo"/>
+</form>
+
+<?php include("../footer.php"); ?>
