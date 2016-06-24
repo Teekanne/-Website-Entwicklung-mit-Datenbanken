5ca642924e3f4bd61371137d56eb92b91db0878d
Kajuuu <kaipiwnicki@googlemail.com>
2016-06-24 18:11:33 +0200

    Update index.php

--- views/newquestion/index.php
+++ views/newquestion/index.php
@@ -22,31 +22,31 @@
 
 <h2>Neue Umfrage erstellen</h2>
 <form action="" method="post">
-    <table id="questionTable">
+    <table border= '0' id="questionTable">
         <tr>
-            <th>Kategorie</th>
+            <th><label>Kategorie</label></th>
             <th><?php Category::ShowSelectBoxWithCategories(); ?></th>
         </tr>
         <tr>
-            <th>Quiz-Name</th>
+            <th><label>Quiz-Name</label></th>
             <th><input type="text" name="quizName" placeholder="Quiz-Name" /></th>
         </tr>
         <tr>
-            <th>Quiz-Beschreibung</th>
+            <th><label>Quiz-Beschreibung</label></th>
             <th><input type="text" name="quizDescription" placeholder="Quiz-Beschreibung" /></th>
         </tr>
         
         <?php for($i=0;$i<20;$i++){ echo "<tr></tr>"; } ?>
         <tr>
-            <th>Frage</th>
+            <th><label>Frage</label></th>
             <th><input type="text" name="question1"/></th>
         </tr>
         <tr>
-            <th>Beschreibung</th>
+            <th><label>Beschreibung</label></th>
             <th><textarea name="description1" maxlength="1000"></textarea></th>
         </tr>
         <tr>
-            <th>Antworten</th>
+            <th><label>Antworten</label></th>
             <th> 
                 <div class="divAnswers1">
                     <input type="text" name="answer1_1" placeholder="Antwortmöglichkeit 1" /><br />
@@ -54,14 +54,22 @@
                 </div>
             </th>
         </tr>
+	
         <tr>
-            <th>Art</th>
+            <th><label>Art</label></th>
             <th>
-                <input type="radio" name="choice1" value="singlechoice" checked="checked"/><label>Single-Choice</label><br />
-                <input type="radio" name="choice1" value="multiplechoice"/><label>Multiple-Choice</label>
+                <input type="radio" name="choice1" value="singlechoice" checked="checked"/>Single-Choice<br />
+                <input type="radio" name="choice1" value="multiplechoice"/>Multiple-Choice
             </th>                            
         </tr>
     </table>
-    
-    <input type="button" value="Neue Frage einfügen" onclick="addNewQuestion('questionTable', 'question1');">  <input type="submit" value="Jetzt erstellen &raquo"/>
+    <br />
+	<table>
+		<tr>
+			<th>
+				<input type="button" value="Neue Frage einfügen" onclick="addNewQuestion('questionTable', 'question1');">  <input type="submit" value="Jetzt erstellen &raquo"/>
+			</th>
+		<tr>
+	</table>
 </form>
+
