46ccf9d892e5b8f749bf9d0d725515fd8120ac4e
chpo7234 <pohl1991@yahoo.de>
2016-07-11 16:12:36 +0200

    Add files via upload

--- public/php/Question.php
+++ public/php/Question.php
@@ -1,5 +1,5 @@
 <?php
-    class Question extends Model {
+    class Question {
         private $id;
         private $description;
         private $qkey;
@@ -22,7 +22,6 @@
         }
 
         public function __construct($id, $description, $qkey, $question, $questionPos, $singleChoice, $fkQuiz){
-            parent::__construct();
             $this->id = $id;
             $this->description = $description;
             $this->qkey = $qkey;
@@ -57,20 +56,42 @@
             return new Question($pdo->lastInsertId(), $description, $quiz->__get("qKey"), $question, $questionPos, $singleChoice, $quiz->__get("id"));
         }
         
-        public static function Load($qkey){
+        public static function LoadByQuiz($quiz, $position){
+            if(!$quiz){
+                return;
+            }
+            
             $pdo = new PDO('mysql:host=projekt.wi.fh-flensburg.de;dbname=projekt2015a', 'projekt2015a', 'P2016s7');
-            $sql= "SELECT * FROM T_QUESTION WHERE QKEY=:key"; 
+            $sql= "SELECT * FROM T_QUESTION WHERE FK_QUIZ=:quiz AND QUESTION_POS=:pos"; 
             $statement = $pdo->prepare($sql);
-            $statement->bindParam(':key', $qkey, PDO::PARAM_STR); 
+            $statement->bindParam(':quiz', $quiz->__get("id"), PDO::PARAM_STR); 
+            $statement->bindParam(':pos', $position, PDO::PARAM_INT); 
             $statement->execute();
-            $result = $statement->fetchAll()[0];
+            $result = $statement->fetchAll();
             
             if(!$result){
-                echo "Frage mit Key '" . $qkey . "' existiert nicht.";
+                echo "Es wurde keine Frage gefunden.";
                 return null;
             }
             
+            $result = $result[0];
             return new Question($result["ID"], $result["DESCRIPTION"], $result["QKEY"], $result["QUESTION"], $result["QUESTION_POS"], $result["ISSINGLECHOICE"], $result["FK_QUIZ"]);
+        }
+        
+        public function IsLastQuestion(){
+            $sql= "SELECT * FROM T_QUESTION WHERE FK_QUIZ=:quiz AND QUESTION_POS=:pos"; 
+            $statement = $this->pdo->prepare($sql);
+            $nextPos = $this->questionPos+1;
+            $statement->bindParam(':quiz', $this->fkQuiz, PDO::PARAM_STR); 
+            $statement->bindParam(':pos', $nextPos, PDO::PARAM_INT); 
+            $statement->execute();
+            $result = $statement->fetchAll();
+            
+            if(!$result){
+                return true;
+            }else{
+                return false;
+            }
         }
         
         public function GetAnswers(){
@@ -107,11 +128,16 @@
         public function ShowTable(){
             $answers = $this->GetAnswers();
             $maxVotes = $this->GetMaxVotes();
-
+            $sumVotes = 0;
+            
+            foreach($answers as $a => $answer){
+                $sumVotes .= $answer->GetVotes();
+            }
+            
             echo "<table><tr>";
 
             if($maxVotes==0){
-                echo "Bisher hat noch niemand abgestimmt.";
+                echo "Bisher hat noch niemand abgestimmt.</tr></table>";
                 return;
             }
 
@@ -143,7 +169,7 @@
                 $currentVotes = $answer->GetVotes();
                 
                 if($currentVotes != 0){
-                    $percent =  $currentVotes / $maxVotes * 100;
+                    $percent =  round($currentVotes / $sumVotes * 100, 1);
                 }else{
                     $percent = 0;
                 }
@@ -155,7 +181,7 @@
 
             /* Antwort in einer Zeile anzeigen */
             foreach($answers as $a => $answer){
-                echo "<td>" . $answer->__get("answer") . "</td>";
+                echo "<td>" . $answer->__get("answer") . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>";
             }
 
             echo "</tr></table>";
