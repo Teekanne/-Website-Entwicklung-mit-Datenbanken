1df92704655e2a2091235ccf5a2783bd01a4aeda
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-11 16:19:00 +0200

    Update index.php

--- views/userediter/index.php
+++ views/userediter/index.php
@@ -13,17 +13,43 @@ $roleEdit = Session::get('ROLEEDIT');
 
 
 <form method="post" action="<?php echo URL; ?>user/create">
-    <h1>Einen bestehenden Benutzer beabreiten</h1></br>
-    <input name="titel" type="text" value="<?php echo $titleEdit ?>" placeholder="Titel"></br>
-    <input name="vorname" type="text" value="<?php echo $firstnameEdit ?>" pattern=".{0}|.{2,20}" required title="Ihr Name muss mindestens zwei maximal 20 Zeichen lang sein"  placeholder="Vorname"></br>
-    <input name="nachname" type="text" value="<?php echo $lastnameEdit ?>" pattern=".{0}|.{2,20}" required title="Ihr Nachname muss mindestestens zwei maximal 20 Zeichen lang sein" placeholder="Nachname"></br>
-    <input name="e-mail" type="email" value="<?php echo $emailEdit ?>" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required title="Bitte geben Sie eine E-Mail Adresse in einem gültigen Format ein" placeholder="E-Mail"></br>
-    <input name="e-mailConfirmation" value="<?php echo $emailEdit ?>" type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required title="Bitte geben Sie eine E-Mail Adresse in einem gültigen Format ein" placeholder="E-Mail bestätigen"></br>
-       <label>Role</label>
-    <select name="ROLE" value="<?php echo $roleEdit ?>">
-        <option value="Default">Default</option>
-        <option value="User">User</option>
-        <option value="Administrator">Administrator</option>
-    </select><br />
-    <input type="submit" name="Benutzer anlegen" value="Registrieren">
+    <h2>Einen bestehenden Benutzer beabreiten</h2>
+	<table border = '0'>
+		<tr>
+			<th><label></label></th>
+			<th><input name="titel" type="text" value="<?php echo $titleEdit ?>" placeholder="Titel"></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><input name="vorname" type="text" value="<?php echo $firstnameEdit ?>" pattern=".{0}|.{2,20}" required title="Ihr Name muss mindestens zwei maximal 20 Zeichen lang sein"  placeholder="Vorname"></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><input name="nachname" type="text" value="<?php echo $lastnameEdit ?>" pattern=".{0}|.{2,20}" required title="Ihr Nachname muss mindestestens zwei maximal 20 Zeichen lang sein" placeholder="Nachname"></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><input name="e-mail" type="email" value="<?php echo $emailEdit ?>" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required title="Bitte geben Sie eine E-Mail Adresse in einem gültigen Format ein" placeholder="E-Mail"></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><input name="e-mailConfirmation" value="<?php echo $emailEdit ?>" type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required title="Bitte geben Sie eine E-Mail Adresse in einem gültigen Format ein" placeholder="E-Mail bestätigen"></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><label id = "role">Role</label></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><select style=width:13.3em; name="ROLE" value="<?php echo $roleEdit ?>">
+				<option value="Default">Default</option>
+				<option value="User">User</option>
+				<option value="Administrator">Administrator</option>
+			</select></th>
+		</tr>
+		<tr>
+			<th><label></label></th>
+			<th><input type="submit" name="Benutzer anlegen" value="Registrieren"></th>
+		</tr>
+	</table>
 </form>
