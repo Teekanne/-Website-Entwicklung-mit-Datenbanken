f0988575ee93b312b073bede49a61073b00dbccb
chpo7234 <pohl1991@yahoo.de>
2016-07-12 19:34:19 +0200

    Update index.php

--- views/result/index.php
+++ views/result/index.php
@@ -3,21 +3,21 @@
         $key = $_GET["key"];
         $disallowed = "<label>Nur der Fragenersteller kann die Ergebnisse sehen ;-)</label>";
         
-        if(isset($_SESSION["ID"])){
-            if($_SESSION["ID"] == Quiz::LoadByKey($key)->__get("fkTutor")){
+        if(isset($_SESSION["ID"]) && Quiz::LoadByKey($key)){
+                if($_SESSION["ID"] == Quiz::LoadByKey($key)->__get("fkTutor")){
            
-                if(isset($_GET["pos"])){
-                    $key .= "&pos=" . $_GET["pos"];
+                    if(isset($_GET["pos"])){
+                        $key .= "&pos=" . $_GET["pos"];
+                    }else{
+                        $key .= "&pos=1";
+                    }
+
+                    echo "<script>showVoteResults('divResults', '" . $key . "', 600);</script>";
+                    echo  "<div id='divResults'><img id='imgAjax' src='images/3.gif'></div>"; 
                 }else{
-                    $key .= "&pos=1";
-                }
-                
-                echo "<script>showVoteResults('divResults', '" . $key . "', 600);</script>";
-                echo  "<div id='divResults'><img id='imgAjax' src='images/3.gif'></div>"; 
-            }else{
-                echo $disallowed;
+                    echo $disallowed;
             }
-        }else{
+        }elseif(Quiz::LoadByKey($key)){
             echo $disallowed;
         }
     }
