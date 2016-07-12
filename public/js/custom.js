1cc0643b81670610cdf72ac9db4bf25e32d55f5f
chpo7234 <pohl1991@yahoo.de>
2016-07-12 14:26:49 +0200

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,7 @@
+/**
+ * Show/hides an user-control
+ * @param  {control} some user-control
+ */
 function hide(control){
     var control = document.getElementById(control);
 
@@ -12,6 +16,11 @@ function hide(control){
     }
 }
 
+/**
+ * Adds another textbox into a div-element
+ * @param {container} container which takes the new textbox
+ * @param {textbox} currentTextbox
+ */
 function addTextbox(container, currentTextbox){
     var currentQuestionNumber = parseInt(container.replace(/^[^\d]+/, ""));
     var nextTextboxNumber = parseInt(currentTextbox.split('_')[1])+1;
@@ -35,20 +44,25 @@ function addTextbox(container, currentTextbox){
     var answers = document.getElementsByClassName(container)[0];
     answers.appendChild(document.createElement("br"));
     answers.appendChild(input);
-    
 }
 
+/**
+ * Adds a complete new container with several 
+ * user-controls for asking another question.
+ * @param {table} table which takes the new user-controls
+ * @param {textbox} textbox with the current question
+ */
 function addNewQuestion(table, currentQuestion){
 
     var myTable = document.getElementById(table);
     var currentQuestionNumber = findLastQuestionNumber() + 1;
     
-    /* Abstand gewinnen */
+    /* Creates few margin */
     for (var i = 0; i < 20; i++) {
         var row = myTable.insertRow(-1);
     }
 
-    /* Creating the DOM */
+    /* Creating of all the DOMs */
     var question = document.createElement('input');
     question.type = 'text';
     question.name = "question" + currentQuestionNumber;
@@ -95,7 +109,7 @@ function addNewQuestion(table, currentQuestion){
     multipleChoiceLabel.innerHTML = "Multiple-Choice";
     multipleChoiceLabel.className = "radioLabel";
     
-    /* Adding the dom */
+    /* Adding all the DOMs */
     var row = myTable.insertRow(-1);
     var cell1 = row.insertCell(0);
     cell1.className = "cells";
@@ -133,6 +147,10 @@ function addNewQuestion(table, currentQuestion){
     cell2.appendChild(multipleChoiceLabel);
 }
 
+/**
+ * Returns the number of the last question
+ * @returns {Number} Number of the last question
+ */
 function findLastQuestionNumber(){
     var lastQuestionNumber = 1;
     
@@ -147,6 +165,13 @@ function findLastQuestionNumber(){
     return lastQuestionNumber;
 }
 
+/**
+ * Shows/hides the password-controls depending on a checkbox-state
+ * @param {checkbox} currentCheckBox which controls the hide/show-function
+ * @param {label-array} passwordLbls which hide/show
+ * @param {textbox-array} currentPasswordTbx which hide/show
+ * @param {textbox} newPasswordTbx which hides/shows
+ */
 function showPasswordBoxes(currentCheckBox, passwordLbls, currentPasswordTbx, newPasswordTbx){
     var lblPasswords = document.getElementsByName(passwordLbls);
 
@@ -167,9 +192,15 @@ function showPasswordBoxes(currentCheckBox, passwordLbls, currentPasswordTbx, ne
     }
 }
 
-function showVoteResults(key){
+/**
+ * Ajax-function to show the vote-results depending on a time
+ * @param {div-container} divContainer which takes the ajax-answer
+ * @param {get-parameters} quiz-key and question-position
+ * @param {integer} intervall for ajax-requests in ms
+ */
+function showVoteResults(divContainer, key, intervall){
         function ajaxRequest() {
-                //erstellen des requests
+                //creating a new request
                 var req = null;
 
                 try {
@@ -192,23 +223,20 @@ function showVoteResults(key){
                 if (req == null) {
                         alert("Error creating request object!");
                 }
-                //anfrage erstellen (GET, url ist localhost,
-                //request ist asynchron
+                
+                //creating an asynchron request
                 var url = 'views/newquestion/results.php?key=' + key;
                 req.open("GET", url, true);
 
-                //Beim abschliessen des request wird diese Funktion ausgeführt
+                //function for terminating the request
                 req.onreadystatechange = function() {
                     switch (req.readyState) {
                         case 4:
                             if (req.status != 200 && req.status != 0) {
                                 alert("Fehler:" + req.status);
                             } else {
-                                //alert(req.responseText);
-                                //schreibe die antwort in den div 
-                                //container mit der id content
                                 var result = '<strong>'+req.responseText+'</strong>';
-                                document.getElementById('eins').innerHTML = result;
+                                document.getElementById(divContainer).innerHTML = result;
                             }
                             break;
 
@@ -222,6 +250,6 @@ function showVoteResults(key){
                 req.send(null);
         }
 
-        //Zeitgesteuerter Aufruf von Ajax
-        setInterval(ajaxRequest, 500);
+        //terminating the ajax-requests on time-intervall
+        setInterval(ajaxRequest, intervall);
 }
