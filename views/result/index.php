c3d7587d14bbdf1b187f9d8298d9c6719c2a4be6
chpo7234 <pohl1991@yahoo.de>
2016-07-12 18:58:54 +0200

    Update index.php

--- views/result/index.php
+++ views/result/index.php
@@ -1,14 +1,24 @@
 <?php 
     if(isset($_GET["key"])){
         $key = $_GET["key"];
+        $disallowed = "<label>Nur der Fragenersteller kann die Ergebnisse sehen ;-)</label>";
         
-        if(isset($_GET["pos"])){
-            $key .= "&pos=" . $_GET["pos"];
+        if(isset($_SESSION["ID"])){
+            if($_SESSION["ID"] == Quiz::LoadByKey($key)->__get("fkTutor")){
+           
+                if(isset($_GET["pos"])){
+                    $key .= "&pos=" . $_GET["pos"];
+                }else{
+                    $key .= "&pos=1";
+                }
+                
+                echo "<script>showVoteResults('divResults', '" . $key . "', 600);</script>";
+                echo  "<div id='divResults'><img id='imgAjax' src='images/3.gif'></div>"; 
+            }else{
+                echo $disallowed;
+            }
         }else{
-            $key .= "&pos=1";
+            echo $disallowed;
         }
-        
-	echo "<script>showVoteResults('divResults', '" . $key . "', 600);</script>";
-        echo  "<div id='divResults'><img id='imgAjax' src='images/3.gif'></div>"; 
     }
  ?>
