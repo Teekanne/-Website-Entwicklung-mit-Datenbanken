e94eeca9d87963ca4cbf1d39483138c96d383234
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-13 21:50:08 +0200

    Update delete.php

--- views/userediter/delete.php
+++ views/userediter/delete.php
@@ -8,11 +8,15 @@ $deleterole = Session::get('DELETEROLE');
 
 echo "<font color='#FFFF00'>Wollen Sie den User mit der ID:".$deleteid."(".$deletetitle." ".$deletefirstname." ".$deletelastname." ".$deleteemail.") mit der Rolle:".$deleterole." wirklich löschen?</font>";
 ?>
-</br>   
-<form method="post" action="<?php echo URL; ?>user/delete">
-<input id="change" type="submit" name="userChange" value="Löschen">
-</form>
-<form method="post" action="<?php echo URL; ?>user/index">
-<input id="change" type="submit" name="userChange" value="Abbrechen">
-</form>
-//
+</br>
+</br>
+<table border='0'>
+	<tr>
+		<td><form method="post" action="<?php echo URL; ?>user/delete">
+		<input id='change' type="submit" name="userChange" value="Löschen">
+		</form></td>
+		<td><form method="post" action="<?php echo URL; ?>user/index">
+		<input id='change' type="submit" name="userChange" value="Abbrechen">
+		</form></td>
+	</tr>
+</table>
