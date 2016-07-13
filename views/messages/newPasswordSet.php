fae7896d32b66579b68ba8578ffc753510deaf0d
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 01:35:20 +0200

    Update newPasswordSet.php

--- views/messages/newPasswordSet.php
+++ views/messages/newPasswordSet.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "<font color='#FFFF00'>Ihr neues Passwort wurde gsetzt und Sie können sich nun damit anmelden.</font>";
+echo "<<label id='messageLabel'></br>>Ihr neues Passwort wurde gsetzt und Sie können sich nun damit anmelden.</label>";
