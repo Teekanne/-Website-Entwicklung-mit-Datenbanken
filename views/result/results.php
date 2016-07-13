cba64f0786870a15e03baa12b122b2a9607f7e5b
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-13 16:22:39 +0200

    Update results.php

--- views/result/results.php
+++ views/result/results.php
@@ -12,37 +12,39 @@
     $myQuestion = Question::LoadByQuiz($myQuiz, $pos);
     
     if($myQuestion){
-        echo "<table>";
+        echo "</br><table>";
         echo "<tr>";
         echo "<td>Quiz:</td>";
-        echo "<td>" . $myQuiz->__get("name") . "</td>";
+        echo "<td>&nbsp;&nbsp&nbsp;&nbsp;" . $myQuiz->__get("name") . "</td>";
         echo "</tr>";
         
         echo "<tr>";
         echo "<td>Quiz-Key:</td>";
-        echo "<td>" . $myQuiz->__get("qKey") . "</td>";
+        echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;" . $myQuiz->__get("qKey") . "</td>";
         echo "</tr>";
         
         if(strlen($myQuiz->__get("description"))>1){
             echo "<tr>";
             echo "<td>Quiz-Beschreibung: </td>";
-            echo "<td>" . $myQuiz->__get("description") . "</td>";
+            echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;" . $myQuiz->__get("description") . "</td>";
             echo "</tr>";
         }
         
         echo "<tr>";
         echo "<td>Frage: </td>";
-        echo "<td>" . $myQuestion->__get("question") . "</td>";
+        echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;" . $myQuestion->__get("question") . "</td>";
         echo "</tr>";
         
         if(strlen($myQuestion->__get("description"))>1){
             echo "<tr>";
-            echo "<td>Fragen-Beschreibung</td>";
-            echo "<td>" . $myQuestion->__get("description") . "</td>";
+            echo "<td>Fragen-Beschreibung:</td>";
+            echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;" . $myQuestion->__get("description") . "</td>";
             echo "</tr>";
         }
         
-        echo "</table>";
+        echo "</table><label id='overviewLabel'>&nbsp;</label><br>";
+		
+		
         
         $myQuestion->ShowTable();
         
@@ -50,11 +52,11 @@
         echo "<input type='text' hidden name='key' value='". $myQuiz->__get("qKey") . "'>";
         echo "<table><tr>";
         if($myQuestion->__get("questionPos") > 1){
-            echo "<td><button type='submit' name='pos' value='" . ($myQuestion->__get("questionPos")-1) . "'    >← Zuvorrige Frage</button></td>";
+            echo "<td><button type='submit' name='pos' value='" . ($myQuestion->__get("questionPos")-1) . "'    >←</button></td>";
         }
         
         if(!$myQuestion->IsLastQuestion()){
-            echo "<td><button type='submit' name='pos' value='" . ($myQuestion->__get("questionPos")+1) . "'>Nächste Frage →</button></td>";
+            echo "<td><button id='next' type='submit' name='pos' value='" . ($myQuestion->__get("questionPos")+1) . "'>→</button></td>";
         }
         echo "</tr></table>";
         echo "</form>";
