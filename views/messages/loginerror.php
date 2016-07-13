15792d40c5098a273111573730b0e83fcd2bdbb1
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 01:34:18 +0200

    Update loginerror.php

--- views/messages/loginerror.php
+++ views/messages/loginerror.php
@@ -1,3 +1,3 @@
 <?php
-echo "<font color='#FFFF00'>Passwort oder Login falsch oder Sie sind noch nicht aktiviert.</font>";
+echo "<label id='messageLabel'></br>Passwort oder Login falsch oder Sie sind noch nicht aktiviert.</label>";
 
