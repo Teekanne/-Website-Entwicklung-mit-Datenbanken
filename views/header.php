916ad59808681a2c50597d8aa0134f855c7dfd4b
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 13:05:02 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -66,10 +66,10 @@
 					<li><a href="<?php echo URL; ?>help">Hilfe</a></li>
 				<?php endif; ?>	
 				<?php if (Session::get('loggedIn') == true):?>
-                                        <?php setUrl("newquestion", "Fragen erstellen"); ?>
-                                        <?php setUrl("overview", "Fragen-Übersicht"); ?>
+                                        <?php setUrl("newquestion", "Quiz erstellen"); ?>
+                                        <?php setUrl("overview", "Quiz-Übersicht"); ?>
                                         <?php setUrl("categoryEdit", "Kategorienverwaltung"); ?>
-                                        <?php setUrl("account", "Account"); ?>
+                                        <?php setUrl("account", "Konto"); ?>
                                         
 				<?php if (Session::get('ROLE') == 'Administrator'):?>
                                     <?php setUrl("user", "Benutzer"); ?>
