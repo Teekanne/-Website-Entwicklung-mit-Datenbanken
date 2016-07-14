4b51ccdb3a80bcdfb9b709a948489759e1cd6267
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 12:19:27 +0200

    Update keysent.php

--- views/messages/keysent.php
+++ views/messages/keysent.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "</br></br></br><label id='messageLabel'></br>Eine Nachricht mit dem Resetkey wurde an ihre E-Mail Adresse geschickt.</label></br></br></br>";
+echo "<br /><br /><br /><label id='messageLabel'><br />Eine Nachricht mit dem Resetkey wurde an ihre E-Mail Adresse geschickt.</label><br /><br /><br />";
