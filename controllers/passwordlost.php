d0d4827425216e59b7684f71fd83d79f1d33652b
Ben <Ben@193.175.177.55>
2016-07-14 12:16:18 +0200

    added email functions.

--- controllers/passwordlost.php
+++ controllers/passwordlost.php
@@ -247,43 +247,6 @@ if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
    // echo "Message sent!";
-}/*
-        
-        
-        
-        
-     // require 'library/PHPMailer/PHPMailerAutoload.php';
-        require 'library/PHPMailer/class.phpmailer.php';
-            require 'library/PHPMailer/class.smtp.php';
-        $mail = new PHPMailer(); 
-$mail->IsSMTP(); // send via SMTP
-
-$mail->SMTPAuth = true; // turn on SMTP authentication
-$mail->Username = "hsflensburg@gmail.com"; // SMTP username
-$mail->Password = "Pegasus123"; // SMTP password
-$mail->SMTPDebug = 1;
-$webmaster_email = "hsflensburg@gmail.com"; //Reply to this email ID
-$email="creq@live.de"; // Recipients email ID
-$name="SomeonesName"; // Recipient's name
-$mail->From = $webmaster_email;
-$mail->FromName = "Me";
-$mail->AddAddress($email,$name);
-$mail->AddReplyTo($webmaster_email,"Webmaster");
-$mail->WordWrap = 50; // set word wrap
-$mail->IsHTML(true); // send as HTML
-$mail->Subject = "This is the subject";
-$mail->Body = "Hi,
-This is the HTML BODY "; //HTML Body
-$mail->AltBody = "This is the body when user views in plain text format"; //Text Body
-if(!$mail->Send())
-{
-    echo "Mailer Error: " . $mail->ErrorInfo;
 }
-else
-{
-    echo "Message has been sent";
-}
-    }
-*/
 }
 }
