b65411ba0d720a7a8843139439540e0206ef3dc9
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 10:43:33 +0200

    Update noemail.php

--- views/messages/noemail.php
+++ views/messages/noemail.php
@@ -1,2 +1,2 @@
 <?php
-echo "<label id='messageLabel'></br>Sie können sich nur mit einer @hs-flensburg.de oder @fh-flensburg.de Adresse anmelden.</label>";
+echo "</br></br></br><label id='messageLabel'></br>Sie können sich nur mit einer @hs-flensburg.de oder @fh-flensburg.de Adresse anmelden.</label></br></br></br>";
