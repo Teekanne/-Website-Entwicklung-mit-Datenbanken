09001bc323419512a1730835bea25c2cff8ed199
chpo7234 <pohl1991@yahoo.de>
2016-07-14 11:23:05 +0200

    Update results.php

--- views/result/results.php
+++ views/result/results.php
@@ -12,6 +12,9 @@
     $myQuestion = Question::LoadByQuiz($myQuiz, $pos);
     
     if($myQuestion){
+        /**
+         * Displaying general informations about the quiz 
+         */
         echo "</br><table>";
         echo "<tr>";
         echo "<td>Quiz:</td>";
@@ -50,8 +53,14 @@
             echo "<h3><center>Anzahl der abgegebenen Stimmen: " . $votes . "</center></h3>";
         }
         
+        /**
+         * Displaying the vote-results
+         */
         $myQuestion->ShowTable();
         
+        /**
+         * Displaying buttons for next- and previous-questions
+         */
         echo "<form method='GET'>";
         echo "<input type='text' hidden name='key' value='". $myQuiz->__get("qKey") . "'>";
         echo "<table><tr>";
