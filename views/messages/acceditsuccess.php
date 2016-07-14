e74f1f01476b6fef09963897ea228ad0c715b536
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 10:39:12 +0200

    Update acceditsuccess.php

--- views/messages/acceditsuccess.php
+++ views/messages/acceditsuccess.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "<label id='messageLabel'></br>Sie haben ihre Daten erfolgreich bearbeitet.</label>";
+echo "</br></br></br><label id='messageLabel'></br>Sie haben ihre Daten erfolgreich bearbeitet.</label></br></br></br>";
