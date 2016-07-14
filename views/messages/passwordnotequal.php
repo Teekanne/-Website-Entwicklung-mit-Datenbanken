5c28a7e472db63a94dbd7a7406a8d79e9e0fba07
Kajuuu <kaipiwnicki@googlemail.com>
2016-07-14 10:43:51 +0200

    Update passwordnotequal.php

--- views/messages/passwordnotequal.php
+++ views/messages/passwordnotequal.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "<label id='messageLabel'></br>Die eingegebenen Passwörter sind nicht identisch.</label>";
+echo "</br></br></br><label id='messageLabel'></br>Die eingegebenen Passwörter sind nicht identisch.</label></br></br></br>";
